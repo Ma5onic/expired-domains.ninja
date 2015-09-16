8ce38647fae2b43b03814d9e32a7294b15aec964
christophebe <christophe.lombart@gmail.com>
2015-09-16 22:16:12 +0200

    fix syntax error

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var Map    = require("collections/fast-map");
-var URI    = require('crawler-ninja/lib/uri');
-var log    = require("crawler-ninja/lib/logger.js");
+var fs              = require('fs');
+var URI             = require('crawler-ninja/lib/uri');
+var log             = require("crawler-ninja/lib/logger.js");
+var domainChecker   = require("check-domain");
 
 
 var ERROR_DNS_LOOKUP = "ENOTFOUND";
-
 var STATUS_DNS_LOOKUP_ERROR = "DNS lookup failed";
-
+var EXPIRED_TXT_FILE = process.cwd() + "/expireds.txt";
 
 /**
  *  Find expired domains
@@ -16,37 +16,45 @@ var STATUS_DNS_LOOKUP_ERROR = "DNS lookup failed";
 function Plugin(crawler) {
 
     this.crawler = crawler;
-    var self = this;
 
-    var expiredLog = log.createLogger("expireds", "./expireds.log");
 
-    //TODO :
-    // For the resources with http status =  50*
-    // 1. Check the root domain
-    // if also in 500 => add to a list in order to check later if it moves to expired
+    var expiredLog = log.createLogger("expireds", {"path" : "./expireds.log"});
 
     this.crawler.on("crawl", function(result,$) {
 
-      //TODO : log only for externals domains
+      // a page with a status of 500+ can be expired
       if (result.statusCode >= 500 && result.statusCode <= 599 ) {
 
-        expiredLog.info({"500" : true, status : result.statusCode, url : result.url});
+        expiredLog.info({"50*" : true, status : result.statusCode, url : result.url});
+        check(URI.domain(result.uri));
+
       }
     });
 
     this.crawler.on("error", function(error, result){
 
-        //TODO : Review this code, need to check other info on the domain :
-        //       is available ?, pending delete ? PageRank ? TrustFlow/Citation Flow, ...  ?
         if (error.code == ERROR_DNS_LOOKUP) {
-
-          var host = URI.host(result.uri);
-          expiredLog.info({expired : true, host : host, url : result.url});
-
+          var domain = URI.domain(result.uri);
+          expiredLog.info({expired : true, domain : domain, url : result.url});
+          check(URI.domain(result.uri));
+        }
+        else {
+          // Just to be sure, catch other errors
+          expiredLog.info({error : true, code : error.code, url : result.url});
         }
 
     });
+
+
 }
 
+check = function(domain) {
+  console.log("Before call domainChecker");
+  domainChecker({domain : domain}, function(error, result){
+      console.log(result);
+      fs.appendFile(EXPIRED_TXT_FILE, result.domain + "," + result.pr + "," + result.available + "," / result.whois);
+      console.log("After call domainChecker");
+  });
+}
 
 module.exports.Plugin = Plugin;
